054997e83e8ce830c7fbbb34c6539568aa5aee50
Miguel Torrejos <migueladrian.torrejos.cics@ust.edu.ph>
2023-11-15 09:18:40 +0800

    add upload functionality

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.recipebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 open class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
-    //lateinit var ingredients: ArrayList<Ingredient>
     lateinit var dishList : ArrayList<Dish>
-    val database = Firebase.database("https://recipe-book-4adb9-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    val database = Firebase.database(//database url)
     val recipeView = RecipeView()
+    val dishRef = database.getReference("dishes")
+    var childCount: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +28,50 @@ open class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
-        recipeView.getRecipes(this,recyclerView)
-        uploadData()
+        recipeView.getRecipes()
+
+        //dishRef child counter
+        dishRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var numberOfChildren = dataSnapshot.childrenCount
+                setCount(numberOfChildren)
+            }
+            override fun onCancelled(databaseError: DatabaseError) { Log.e("Error:", databaseError.message) }
+        })
+
+
+        dishList = arrayListOf()
+
+        findViewById<Button>(R.id.button).setOnClickListener(){
+            uploadData()
+        }
+
+        findViewById<Button>(R.id.updatebtn).setOnClickListener(){
+            val dishChildCreate = dishRef.child("dishID: " + (childCount-1).toString()).ref //get reference to dishID: #, which would create the db entry
+            dishChildCreate.setValue("HELLO WORLD")
+        }
     }
 
-    fun uploadData() {
-        var ingredients : ArrayList<Ingredient> = arrayListOf()
-        ingredients.add(Ingredient("potato", 5, "grams"))
-        ingredients.add(Ingredient("tomato", 20, "grams"))
-        val testDish = Dish("test","test recipe", ingredients, R.drawable.ic_launcher_background)
-        val myRef = database.getReference("dishes")
+fun uploadData() {
+    var ingredients : ArrayList<Ingredient> = arrayListOf()
+    try {
 
-        myRef.setValue("Recipe Book")
-        myRef.child(testDish.name).setValue(testDish)
+        val testDish = Dish("dishID: "+ (childCount+1).toString(),"test recipe", ingredients, R.drawable.ic_launcher_background) //dish details
+        //val dishChildCreate = dishRef.push() //reference child creation, push() is the function that creates the child
+        //dishChildCreate.child((childCount++).toString()).setValue(testDish) //creates the actual child with specified path and sets the value
+
+        //below is arguably the better way to do it
+        val dishChildCreate = dishRef.child("dishID: " + childCount++.toString()).ref //get reference to dishID: #, which would create the db entry
+        dishChildCreate.setValue(testDish) //actual creation of db entry
+    }
+
+    catch (e:Exception) {
+        Log.e("error", "error")
+    }
+}
+
+    private fun setCount(i: Long) { //just sets the child count for the main view
+        childCount = i
     }
 }
 
--- app/src/main/java/com/example/recipebook/RecipeView.kt
+++ app/src/main/java/com/example/recipebook/RecipeView.kt
@@ -1,45 +1,38 @@
 package com.example.recipebook
 
-import android.content.Context
 import android.util.Log
-import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class RecipeView() {
-
-    fun getRecipes(context: Context, recyclerView: RecyclerView) {
+class RecipeView {
+     fun getRecipes(){
         try {
-            //lateinit var ingredients: ArrayList<Ingredient>
-            lateinit var dishList: ArrayList<Dish>
-            val database = Firebase.database(/*link to database*/)
-            var myRef = database.getReference("message")
+            val database = Firebase.database("https://recipe-book-4adb9-default-rtdb.asia-southeast1.firebasedatabase.app/")
+            val recipeRef = database.reference.child("dishes")
 
-            myRef.addValueEventListener(object : ValueEventListener {
+            recipeRef.addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    dishList = arrayListOf()
+                    // Iterate through the child nodes and retrieve their names
+                    for (childSnapshot in dataSnapshot.children) {
+                        val recipeName = childSnapshot.key
+                        Log.d("HELP", recipeName!!)
 
-                    for (recipe in dataSnapshot.children) {
-                        val dish: Dish? = dataSnapshot.getValue(Dish::class.java)
-                        dish?.let { dishList.add(it) }
+                        val image = childSnapshot.child("image").value
+
+                        Log.d("HELP IMAGE", image.toString())
                     }
-                    recyclerView.adapter = AdapterClass(dishList)
-
+                    Log.d("HELP COUNT", dataSnapshot.childrenCount.toString())
                 }
 
-                override fun onCancelled(error: DatabaseError) {
-                    Log.e("error", "Failed to read value.", error.toException())
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.e("Error", "onCancelled", databaseError.toException())
                 }
             })
-
-
-
-
         } catch (e: Exception) {
-            Log.e("error", "Something went wrong...")
+            Log.e("error", "Something went wrong...", e)
         }
     }
 }
